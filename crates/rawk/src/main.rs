efde23d1846dc91282c3b6ee0c000210268b0eff
Stefan Alfbo <stefan@alfbo.se>
2026-01-03 09:44:22 +0100

    Start building on the cli part of the rawk project

--- crates/rawk/src/main.rs
+++ crates/rawk/src/main.rs
@@ -1,3 +1,22 @@
+use rawk_core::lexer::Lexer;
+use rawk_core::parser::Parser;
+
 fn main() {
-    println!("Hello, rawk!");
+    let awk_script = std::env::args()
+        .nth(1)
+        .expect("Please provide an AWK script");
+    let path = std::env::args().nth(2).expect("Please provide a file path");
+
+    println!("AWK Script: {}", awk_script);
+    println!("File Path: {}", path);
+
+    execute(&awk_script, &path)
+}
+
+fn execute(awk_script: &str, _path: &str) {
+    let lexer = Lexer::new(awk_script);
+    let mut parser = Parser::new(lexer);
+    let program = parser.parse_program();
+
+    println!("Parsed Program: {}", program);
 }
