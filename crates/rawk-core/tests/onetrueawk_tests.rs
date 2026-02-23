94fc770bc56caf4c7f570c3dbf20dbd1b89b98ec
Stefan Alfbo <stefan@alfbo.se>
2026-02-23 21:55:02 +0100

    Refactor test cases to use a common assertion function for output validation

--- crates/rawk-core/tests/onetrueawk_tests.rs
+++ crates/rawk-core/tests/onetrueawk_tests.rs
@@ -1,11 +1,6 @@
 use rawk_core::awk::Awk;
 
-#[test]
-fn p1() {
-    let script = include_str!("onetrueawk-testdata/p.1");
-    let data = include_str!("onetrueawk-testdata/countries");
-    let expected_data = include_str!("onetrueawk-testdata/p.1.expected");
-
+fn assert_script_output_matches(script: &str, data: &str, expected_data: &str) {
     let input: Vec<String> = data.lines().map(str::to_string).collect();
     let expected: Vec<String> = expected_data.lines().map(str::to_string).collect();
 
@@ -16,18 +11,21 @@ fn p1() {
 }
 
 #[test]
+fn p1() {
+    let script = include_str!("onetrueawk-testdata/p.1");
+    let data = include_str!("onetrueawk-testdata/countries");
+    let expected_data = include_str!("onetrueawk-testdata/p.1.expected");
+
+    assert_script_output_matches(script, data, expected_data);
+}
+
+#[test]
 fn p2() {
     let script = include_str!("onetrueawk-testdata/p.2");
     let data = include_str!("onetrueawk-testdata/countries");
     let expected_data = include_str!("onetrueawk-testdata/p.2.expected");
 
-    let input: Vec<String> = data.lines().map(str::to_string).collect();
-    let expected: Vec<String> = expected_data.lines().map(str::to_string).collect();
-
-    let awk = Awk::new(script);
-    let output = awk.run(input);
-
-    assert_eq!(output, expected);
+    assert_script_output_matches(script, data, expected_data);
 }
 
 #[test]
@@ -37,13 +35,7 @@ fn p3() {
     let data = include_str!("onetrueawk-testdata/countries");
     let expected_data = include_str!("onetrueawk-testdata/p.3.expected");
 
-    let input: Vec<String> = data.lines().map(str::to_string).collect();
-    let expected: Vec<String> = expected_data.lines().map(str::to_string).collect();
-
-    let awk = Awk::new(script);
-    let output = awk.run(input);
-
-    assert_eq!(output, expected);
+    assert_script_output_matches(script, data, expected_data);
 }
 
 #[test]
@@ -52,11 +44,5 @@ fn p4() {
     let data = include_str!("onetrueawk-testdata/countries");
     let expected_data = include_str!("onetrueawk-testdata/p.4.expected");
 
-    let input: Vec<String> = data.lines().map(str::to_string).collect();
-    let expected: Vec<String> = expected_data.lines().map(str::to_string).collect();
-
-    let awk = Awk::new(script);
-    let output = awk.run(input);
-
-    assert_eq!(output, expected);
+    assert_script_output_matches(script, data, expected_data);
 }
