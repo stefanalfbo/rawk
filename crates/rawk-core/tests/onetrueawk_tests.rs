32eda27c50a9c7f83a8bac97bfc24c4ff59a0b0e
Stefan Alfbo <stefan@alfbo.se>
2026-02-23 21:52:47 +0100

    Add test case for p.4 and corresponding expected output

--- crates/rawk-core/tests/onetrueawk_tests.rs
+++ crates/rawk-core/tests/onetrueawk_tests.rs
@@ -45,3 +45,18 @@ fn p3() {
 
     assert_eq!(output, expected);
 }
+
+#[test]
+fn p4() {
+    let script = include_str!("onetrueawk-testdata/p.4");
+    let data = include_str!("onetrueawk-testdata/countries");
+    let expected_data = include_str!("onetrueawk-testdata/p.4.expected");
+
+    let input: Vec<String> = data.lines().map(str::to_string).collect();
+    let expected: Vec<String> = expected_data.lines().map(str::to_string).collect();
+
+    let awk = Awk::new(script);
+    let output = awk.run(input);
+
+    assert_eq!(output, expected);
+}
