2ddf9200594d5bae342926608ff9ec7ba7b5cb3f
Stefan Alfbo <stefan@alfbo.se>
2026-03-02 20:17:53 +0100

    Enable test case for t1x and update to use t_test macro

--- crates/rawk-core/tests/t_tests.rs
+++ crates/rawk-core/tests/t_tests.rs
@@ -40,7 +40,7 @@ macro_rules! t_test_ignore {
 t_test!(t0, "0");
 t_test!(t0a, "0a");
 t_test!(t1, "1");
-t_test_ignore!(t1x, "1.x");
+t_test!(t1x, "1.x");
 t_test_ignore!(t2, "2");
 t_test_ignore!(t2x, "2.x");
 t_test_ignore!(t3, "3");
