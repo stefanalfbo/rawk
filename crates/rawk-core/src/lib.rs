69c943741266cfb176a7bc78a9011bbef6ae45c3
Stefan Alfbo <stefan@alfbo.se>
2026-03-20 22:57:35 +0100

    refactor(parser): separate ParseError and ParseErrorKind into their own module

--- crates/rawk-core/src/lib.rs
+++ crates/rawk-core/src/lib.rs
@@ -1,11 +1,13 @@
 pub use ast::{Action, Expression, Program, Rule};
 pub use evaluator::Evaluator;
 pub use lexer::Lexer;
-pub use parser::{ParseError, ParseErrorKind, Parser};
+pub use parse_error::{ParseError, ParseErrorKind};
+pub use parser::Parser;
 
 mod ast;
 pub mod awk;
 pub mod evaluator;
 pub mod lexer;
+mod parse_error;
 pub mod parser;
 mod token;
--- crates/rawk-core/src/parser.rs
+++ crates/rawk-core/src/parser.rs
@@ -1,63 +1,9 @@
+pub use crate::parse_error::{ParseError, ParseErrorKind};
 use crate::{
     Lexer, Program,
     ast::{Action, Expression, FunctionDefinition, Rule, Statement},
     token::{Token, TokenKind},
 };
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ParseErrorKind {
-    ExpectedRule,
-    ExpectedStatement,
-    ExpectedIdentifier,
-    ExpectedLeftBrace,
-    ExpectedRightParen,
-    MissingPrintfFormatString,
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct ParseError<'a> {
-    pub kind: ParseErrorKind,
-    pub token: Token<'a>,
-}
-
-impl std::fmt::Display for ParseError<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self.kind {
-            ParseErrorKind::ExpectedRule => write!(
-                f,
-                "unexpected token {:?} ({:?}) at byte {}: expected rule",
-                self.token.kind, self.token.literal, self.token.span.start
-            ),
-            ParseErrorKind::ExpectedStatement => write!(
-                f,
-                "unexpected token {:?} ({:?}) at byte {}: expected statement",
-                self.token.kind, self.token.literal, self.token.span.start
-            ),
-            ParseErrorKind::ExpectedIdentifier => write!(
-                f,
-                "unexpected token {:?} ({:?}) at byte {}: expected identifier",
-                self.token.kind, self.token.literal, self.token.span.start
-            ),
-            ParseErrorKind::ExpectedLeftBrace => write!(
-                f,
-                "unexpected token {:?} ({:?}) at byte {}: expected left brace",
-                self.token.kind, self.token.literal, self.token.span.start
-            ),
-            ParseErrorKind::ExpectedRightParen => write!(
-                f,
-                "unexpected token {:?} ({:?}) at byte {}: expected right paren",
-                self.token.kind, self.token.literal, self.token.span.start
-            ),
-            ParseErrorKind::MissingPrintfFormatString => write!(
-                f,
-                "printf requires a format string at byte {}",
-                self.token.span.start
-            ),
-        }
-    }
-}
-
-impl std::error::Error for ParseError<'_> {}
 
 #[derive(Debug)]
 pub struct Parser<'a> {
